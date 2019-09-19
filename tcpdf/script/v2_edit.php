84cc2da5d4e1ad64b3c82c7d5d18e1cf08c8edad
davidoso <jdavidosori96@gmail.com>
2019-09-19 16:56:51 -0500

    Translation (additional task)

--- tcpdf/script/v2_edit.php
+++ tcpdf/script/v2_edit.php
@@ -60,50 +60,61 @@ $pdf->Ln(10);
 $pdf->SetFont('helvetica', '', 12);
 
 
-
 // Create FILLABLE / VIEW-ONLY fields
 
 
-// PENDIENTE: COMENTAR NOMBRES A CADA CAMPO
 // First name
 $pdf->SetFillColor(233, 236, 239);
-$pdf->Cell(40, 10, 'First name ', 0, 0, 'R', 1);
+$pdf->setCellPaddings(0, 0, 2, 0);
+$pdf->Cell(40, 10, 'First name', 0, 0, 'R', 1);
 $pdf->setCellPaddings(2, 2, 0, 0);
-$pdf->TextField('firstname', 120, 10, array(), array('v'=>$data[0]->value, 'dv'=>$data[0]->value));
+$pdf->TextField('firstname', 140, 10, array(), array('v'=>$data[0]->value, 'dv'=>$data[0]->value));
 $pdf->Ln(10);
 
+// Date
 $pdf->SetFillColor(233, 236, 239);
-$pdf->Cell(40, 10, 'Date ', 0, 0, 'R', 1);
+$pdf->setCellPaddings(0, 0, 2, 0);
+$pdf->Cell(40, 10, 'Date', 0, 0, 'R', 1);
 $pdf->setCellPaddings(2, 2, 0, 0);
-$pdf->Cell(120, 10, $data[1]->value, 0, 0, 'L', 0);
+$pdf->Cell(140, 10, $data[1]->value, 0, 0, 'L', 0);
 $pdf->Ln(10);
 
+// Geolocation
 $pdf->SetFillColor(233, 236, 239);
-$pdf->Cell(40, 10, 'Geo Location ', 0, 0, 'R', 1);
+$pdf->setCellPaddings(0, 0, 2, 0);
+$pdf->Cell(40, 10, 'Geo Location', 0, 0, 'R', 1);
 $pdf->setCellPaddings(2, 2, 0, 0);
-$pdf->TextField('geolocation', 120, 10, array(), array('v'=>$data[2]->value, 'dv'=>$data[2]->value));
+$pdf->TextField('geolocation', 140, 10, array(), array('v'=>$data[2]->value, 'dv'=>$data[2]->value));
 $pdf->Ln(10);
 
+// Email
 $pdf->SetFillColor(233, 236, 239);
-$pdf->Cell(40, 10, 'Email ', 0, 0, 'R', 1);
+$pdf->setCellPaddings(0, 0, 2, 0);
+$pdf->Cell(40, 10, 'Email', 0, 0, 'R', 1);
 $pdf->setCellPaddings(2, 2, 0, 0);
-$pdf->TextField('email', 120, 10, array(), array('v'=>$data[3]->value, 'dv'=>$data[3]->value));
+$pdf->Cell(140, 10, $data[3]->value, 0, 0, 'L', 0);
 $pdf->Ln(10);
 
+// Password
 $pdf->SetFillColor(233, 236, 239);
-$pdf->Cell(40, 10, 'Password ', 0, 0, 'R', 1);
+$pdf->setCellPaddings(0, 0, 2, 0);
+$pdf->Cell(40, 10, 'Password', 0, 0, 'R', 1);
 $pdf->setCellPaddings(2, 2, 0, 0);
-$pdf->TextField('password', 120, 10, array(), array('v'=>$data[4]->value, 'dv'=>$data[4]->value));
+$pdf->Cell(140, 10, $data[4]->value, 0, 0, 'L', 0);
 $pdf->Ln(10);
 
+// Number
 $pdf->SetFillColor(233, 236, 239);
-$pdf->Cell(40, 10, 'Number ', 0, 0, 'R', 1);
+$pdf->setCellPaddings(0, 0, 2, 0);
+$pdf->Cell(40, 10, 'Number', 0, 0, 'R', 1);
 $pdf->setCellPaddings(2, 2, 0, 0);
-$pdf->TextField('number', 120, 10, array(), array('v'=>$data[5]->value, 'dv'=>$data[5]->value));
+$pdf->TextField('number', 140, 10, array(), array('v'=>$data[5]->value, 'dv'=>$data[5]->value));
 $pdf->Ln(10);
 
+// Photo upload
 $pdf->SetFillColor(233, 236, 239);
-$pdf->Cell(40, 40, 'Photo upload ', 0, 0, 'R', 1);
+$pdf->setCellPaddings(0, 0, 2, 0);
+$pdf->Cell(40, 40, 'Photo upload', 0, 0, 'R', 1);
 $x = 60;
 $pdf->Image('https://19b5bf454221a3009503-837ce6cc4941d82f7e9704a4735379b2.ssl.cf1.rackcdn.com/ffc3279b663d.jpg', $x, '', 30, 40);
 $x += 40;
@@ -112,16 +123,18 @@ $x += 40;
 $pdf->Image('https://19b5bf454221a3009503-837ce6cc4941d82f7e9704a4735379b2.ssl.cf1.rackcdn.com/833cd79c2f53.jpg', $x, '', 30, 40);
 $pdf->Ln(40);
 
+// Select Multiple
 $pdf->SetFillColor(233, 236, 239);
-$pdf->Cell(40, 30, 'Select Multiple ', 0, 0, 'R', 1);
-//$pdf->setCellPaddings(2, 2, 0, 0);
-$pdf->ListBox('listbox', 120, 30, array('Blue', 'Green'), array('multipleSelection'=>'true'));
+$pdf->setCellPaddings(0, 0, 2, 0);
+$pdf->Cell(40, 30, 'Select Multiple', 0, 0, 'R', 1);
+$pdf->setCellPaddings(2, 2, 0, 0);
+$pdf->ListBox('selectmultiple', 140, 30, array('Blue', 'Green'), array('multipleSelection'=>'true'));
 $pdf->Ln(60);
 
 $pdf->SetX(50);
 
 // Button to validate and print
-$pdf->Button('print', 30, 10, 'Print', 'Print()', array('lineWidth'=>2, 'borderStyle'=>'beveled', 'fillColor'=>array(128, 196, 255), 'strokeColor'=>array(64, 64, 64)));
+$pdf->Button('print', 30, 10, 'Print', 'print()', array('lineWidth'=>2, 'borderStyle'=>'beveled', 'fillColor'=>array(128, 196, 255), 'strokeColor'=>array(64, 64, 64)));
 
 // Reset button
 $pdf->Button('reset', 30, 10, 'Reset', array('S'=>'ResetForm'), array('lineWidth'=>2, 'borderStyle'=>'beveled', 'fillColor'=>array(128, 196, 255), 'strokeColor'=>array(64, 64, 64)));
@@ -131,7 +144,7 @@ $pdf->Button('submit', 30, 10, 'Submit', array('S'=>'SubmitForm', 'F'=>'http://l
 
 // Form validation functions
 $js = <<<EOD
-function CheckField(name,message) {
+function checkField(name,message) {
 	var f = getField(name);
 	if(f.value == '') {
 	    app.alert(message);
@@ -140,11 +153,11 @@ function CheckField(name,message) {
 	}
 	return true;
 }
-function Print() {
-	if(!CheckField('firstname','First name is mandatory')) {return;}
-	if(!CheckField('lastname','Last name is mandatory')) {return;}
-	if(!CheckField('gender','Gender is mandatory')) {return;}
-	if(!CheckField('address','Address is mandatory')) {return;}
+function print() {
+	if(!checkField('firstname', 'First name is required')) {return;}
+	if(!checkField('geolocation', 'Geolocation is required')) {return;}
+	if(!checkField('number', 'Number is required')) {return;}
+	if(!checkField('selectmultiple', 'Select Multiple is required')) {return;}
 	print();
 }
 EOD;
